8e4250a7cc7972ecc9af0495ea1af572f023349e
Christian Montes <christian.montes8991@gmail.com>
2021-04-23 02:12:24 -0700

    added new user input logic

--- components/register.js
+++ components/register.js
@@ -1,10 +1,56 @@
-import { useState } from 'react';
+import { useState, useCallback } from 'react';
 import RegisterButton from './register-buttons';
+import axios from 'axios';
+import { eventListeners } from '@popperjs/core';
+
 
 export default function Register() {
 
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
+  const [style, setStyle] = useState({border: '1px solid lightgrey'})
+
+  async function registerUser(event) {
+    event.preventDefault();
+    
+    const response = await axios({
+      method: 'POST',
+      url: '/api/register',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      data: {
+        username: username,
+        password: password
+      }
+    }).then(
+      r => {r.json()}
+    );
+
+    // const { usernameTaken } = response;
+
+  }
+
+  const checkUsername = async event => {
+    event.preventDefault();
+    setUsername(event.target.value)
+
+    const response = await axios({
+      method: 'GET',
+      url: '/api/register',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      data: {
+        username: event.target.value
+      }
+    });
+
+    console.log(response)
+    const { usernameTaken } = response.data;
+
+    usernameTaken && (setStyle({border: '2px solid red'}))
+  }
 
   return (
     <>
@@ -15,26 +61,27 @@ export default function Register() {
           <div className='col-sm'>
             <div className='form-floating mb-2'>
               <input 
-              defaultValue={username}
-              onChange={(e) => setUsername(e.target.value)}
-              type='username' 
-              className='form-control' 
-              id='floating-username'
-              placeholder='new-username'
+                defaultValue={username}
+                style={style}
+                onChange={checkUsername}
+                type='username' 
+                className='form-control' 
+                id='new-username'
+                placeholder='Username'
               />
-              <label for='new-username'>Username</label>
+              <label htmlFor='new-username'>Username</label>
             </div>
 
             <div className='form-floating mb-2'>
               <input 
-              defaultValue={password}
-              onChange={(e) => setPassword(e.target.value)}
-              type='password' 
-              className='form-control' 
-              id='password'
-              placeholder='Password'
+                defaultValue={password}
+                onChange={(e) => setPassword(e.target.value)}
+                type='password' 
+                className='form-control' 
+                id='password'
+                placeholder='Password'
               />
-              <label for='password'>Password</label>
+              <label htmlFor='password'>Password</label>
             </div>
 
             <div className='d-grid gap-2'>
