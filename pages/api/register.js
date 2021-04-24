085946c468198cf002c8b9169675f426d26eeb02
Christian Montes <christian.montes8991@gmail.com>
2021-04-23 20:59:53 -0700

    connected db to backend

--- pages/api/register.js
+++ pages/api/register.js
@@ -1,6 +1,41 @@
 import connectToDatabase from '../../utils/db-connection';
 
-export default function handler(req, res) {
+export default async function handler(req, res) {
 
-  res.json({usernameTaken: true});
+  // cached mongo db instance used
+  const { db } = await connectToDatabase();
+  // console.log(req.method);
+
+  return new Promise(resolve => {
+    switch (req.method) {
+      case 'GET': {
+        try {
+          const { username: user } = req.query;
+
+          db.findOne({username: user}, 
+            (err, user) => {
+              return err ? (console.error(err))
+              : (user) ? (res.status(200).json({usernameTaken: true}), resolve())
+              : (res.status(200).json({usernameTaken: false}), resolve())
+            })
+        } catch (error) {
+          console.error('Database error');
+          res.status(500).json({usernameTaken: 'error'});
+          return resolve();
+        }
+      }
+
+      case 'POST': {
+        // statements
+      }
+    }
+    
+    // res.status(405).end();
+    // return resolve();
+  })
+
+
+  // console.log(user);
+  
+  // res.json({usernameTaken: true});
 }
