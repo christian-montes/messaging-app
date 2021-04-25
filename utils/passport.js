e0342449ca127a1be17115ee31423bccb6fc1405
Christian Montes <christian.montes8991@gmail.com>
2021-04-25 01:15:13 -0700

    added local authentication strategy

---
+++ utils/passport.js
@@ -0,0 +1,39 @@
+import passport from 'passport';
+import LocalStrategy from 'passport-local';
+import { findUserById, findUserByUsername, validatePassword } from './db';
+
+// serialize user for session
+passport.serializeUser((user, done) => {
+
+  // using the unique id from MONGO DB
+  done(null, user._id);
+});
+
+// MAY NEED TO REMOVE req FROM FUNCTION CALLS
+// LOOKS TO ONLY BE USED FOR MOCK DB CALLS
+passport.deserializeUser((req, id, done) => {
+
+  // findUserByUsername function from .db
+  // deserialize the user and return the user object stored in DB
+  const user = await findUserById(req, id);
+  done(null, user)
+});
+
+
+// setting up LocalStrategy
+passport.use(
+  new LocalStrategy(
+    {passReqToCallback: true},
+    (req, username, password, done) => {
+      // first find user in DB
+      // passing the req object to be destructured by the function
+      const user = findUserByUsername(req, username);
+      if (!user || !validatePassword(user, password)) {
+        done(null, null);
+      } else {
+        // if the user exists and the password matches return the user object
+        done(null, user);
+      }
+    }
+  )
+)
