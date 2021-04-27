f6f0000f700a67d992241387d3e27568986e21a0
Christian Montes <christian.montes8991@gmail.com>
2021-04-27 02:28:18 -0700

    Some functions need changes to db calls

--- utils/db.js
+++ utils/db.js
@@ -2,8 +2,6 @@ import connectToDatabase from './db-connection';
 import argon2 from 'argon2';
 import { ObjectID } from 'mongodb';
 
-const { db } = connectToDatabase();
-
 /**
  * createUser will be used on sign up
  * @password will be hashed using argon2i variant
@@ -13,7 +11,15 @@ const { db } = connectToDatabase();
  * default value by argon2 is unset; 
  * generates a cryptographically safe random salt
  */
-export async function createUser(req, { username, password, name }) {
+
+ /**
+  * Function hashes password and registers user.
+  * Returns new user object created
+  * @param {Object} param0 Request object containing username
+  * password, and name supplied.
+  */
+export async function createUser({ username, password, name }) {
+  const { db } = connectToDatabase();
   const hash = await argon2.hash(password, {
     type: argon2.argon2i
   });
@@ -33,6 +39,7 @@ export async function createUser(req, { username, password, name }) {
  * for deserialization by passport
  */
 export async function findUserById(id) {
+  const { db } = connectToDatabase();
 
   await db.findOne({_id: new ObjectID(id)}, (err, doc) => {
     return doc;
@@ -40,14 +47,19 @@ export async function findUserById(id) {
 };
 
 /**
- * finding the object by username for 
- * local authentication strategy
+ * Searches MongoDB for user document and returns document
+ * @param {String} username Username entered in sign up or login form
  */
 export async function findUserByUsername(username) {
+  const { db } = await connectToDatabase();
+  let user
 
   await db.findOne({username: username}, (err, doc) => {
-    return doc
+    if (err) return err;
+    user = doc;
   })
+  console.log(user)
+  return user
 }
 
 export async function validatePassword({ hash }, passwordEntered) {
