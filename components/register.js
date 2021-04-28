15231c6b89f4297bd9134650e915a7dcc65a9757
Christian Montes <christian.montes8991@gmail.com>
2021-04-27 22:52:20 -0700

    Completed Local Strategy signup; Needs testing

--- components/register.js
+++ components/register.js
@@ -6,7 +6,8 @@ export default function Register() {
 
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
-  const [style, setStyle] = useState({border: '1px solid lightgrey'})
+  const [style, setStyle] = useState({border: '1px solid lightgrey'});
+  const [message, setMessage] = useState('');
 
   const registerUser = async event => {
     event.preventDefault();
@@ -31,7 +32,7 @@ export default function Register() {
     event.preventDefault();
     setUsername(event.target.value)
 
-    const response = await axios({
+    const response = event.target.value && await axios({
       // GET requests send params to backend to check if username is taken
       method: 'GET',
       url: '/api/register',
@@ -45,11 +46,18 @@ export default function Register() {
 
     // console.log(response)
     const { status } = response;
-    if (status === 201) {
-      setStyle({border: '2px solid green'});
-    } else {
-      setStyle({border: '2px solid red'})
-    }
+    (!status) ? (
+      setStyle({border: '1px solid lightgrey'}),
+      setMessage('')
+    )
+    : (status === 201) ? (
+      setStyle({border: '2px solid green'}),
+      setMessage('Username available')
+    ) 
+    : (
+      setStyle({border: '2px solid red'}),
+      setMessage('Username not available')
+    )
 
   }
 
@@ -58,47 +66,47 @@ export default function Register() {
       <h2>Register an account</h2>
 
       <div className='container'>
-        <div className='row'>
-          <form className='col-sm' onSubmit={registerUser}>
-            <div className='form-floating mb-2'>
-              <input 
-                defaultValue={username}
-                name='username'
-                style={style}
-                onChange={checkUsername}
-                type='username' 
-                className='form-control' 
-                id='username'
-                placeholder='Username'
-              />
-              <label htmlFor='username'>Username</label>
-            </div>
-
-            <div className='form-floating mb-2'>
-              <input 
-                defaultValue={password}
-                name='password'
-                onChange={(e) => setPassword(e.target.value)}
-                type='password' 
-                className='form-control' 
-                id='password'
-                placeholder='Password'
-              />
-              <label htmlFor='password'>Password</label>
-            </div>
-
-            <div className='d-grid gap-2'>
-              <button 
-                className='btn btn-success' 
-                type='submit' 
-                disabled={!(username && password.length >= 5)}>Register
-              </button>
-            </div>
-          </form>
-          <div className='d-grid gap-2 col-sm divider'>
-            <RegisterButton google />
-            <RegisterButton />
+        <div className='row mx-1'>{message ? message : ''}</div>
+        <form className='col-sm-8 mb-3' onSubmit={registerUser}>
+          <div className='form-floating mb-2'>
+            <input
+              defaultValue={username}
+              name='username'
+              style={style}
+              onChange={checkUsername}
+              type='username' 
+              className='form-control' 
+              id='username'
+              placeholder='Username'
+            />
+            <label htmlFor='username'>Username</label>
           </div>
+
+          <div className='form-floating mb-2'>
+            <input 
+              defaultValue={password}
+              name='password'
+              onChange={(e) => setPassword(e.target.value)}
+              type='password' 
+              className='form-control' 
+              id='password'
+              placeholder='Password'
+            />
+            <label htmlFor='password'>Password</label>
+          </div>
+
+          <div className='d-grid gap-2'>
+            <button 
+              className='btn btn-success' 
+              type='submit' 
+              disabled={!(username && password.length >= 5)}>Register
+            </button>
+          </div>
+        </form>
+        <hr className='col-sm-8'/>
+        <div className='d-grid gap-1 col-sm-8'>
+          <RegisterButton google />
+          <RegisterButton />
         </div>
       </div>
     </>
