392632b9192b4bb4f747815060ee375a172b01a9
Christian Montes <christian.montes8991@gmail.com>
2021-05-02 00:29:35 -0700

    Added Login functionality with Google/GitHub buttons

--- pages/index.js
+++ pages/index.js
@@ -2,18 +2,31 @@ import Head from 'next/head';
 import styles from '../styles/Home.module.css';
 import Layout from '../components/layout';
 import LoginForm from '../components/login';
+import RegisterButton from '../components/register-buttons';
+import Register from '../components/register';
 
 export default function Home() {
   return (
     <Layout>
-      <LoginForm />
+      <div className='container'>
+        <div className='col-sm-8'>
+          <LoginForm />
+          
+          <hr />
 
-      <hr />
+          <div className='d-grid gap-1 col-sm mb-4'>
+            <RegisterButton google login />
+            <RegisterButton login />
+          </div>
 
-      <p>
-        Don't have an account?{' '}
-        <a href='/new-user'>Register here.</a>
-      </p>
+          <p>
+            Don't have an account?{' '}
+            <a href='/new-user'>Register here.</a>
+          </p>
+          
+        </div>
+      </div>
+
 
     </Layout>
   )
