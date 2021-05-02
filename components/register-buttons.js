34f5048d95563479df3538c9459e0a4ef7c470af
Christian Montes <christian.montes8991@gmail.com>
2021-05-01 23:54:30 -0700

    Added functionality to OAuth buttons

--- components/register-buttons.js
+++ components/register-buttons.js
@@ -1,12 +1,33 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faGoogle, faGithubSquare } from  '@fortawesome/free-brands-svg-icons';
+import axios from 'axios';
 
 export default function RegisterButton({ google }) {
+
+  const googleAuth = async event => {
+    event.preventDefault();
+
+    axios.get(
+      '/api/auth/google'
+    )
+  }
+
+  const githubAuth = async event => {
+    event.preventDefault();
+
+    axios.get(
+      '/api/auth/github'
+    )
+  }
+
+
   return (
     <>
       <button 
         className={google ? 'btn google-button' : 'btn github-button'}
-        type='button'>
+        type='button'
+        onClick={google ? googleAuth : githubAuth}
+      >
           <span className='bigger-icon'><FontAwesomeIcon icon={google ? faGoogle : faGithubSquare} /></span>{' '}
           {google ? 'Sign up with Google' : 'Sign up with GitHub'}
       </button>
